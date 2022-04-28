f15c57f447b3ace26b007c094a959097e6f58877
nirmal-mazhuvanpilly <nirmalmazhuvanpilly@gmail.com>
2022-04-28 11:38:03 +0530

    Android back button handling

--- lib/utils/navigators.dart/home_navigator.dart
+++ lib/utils/navigators.dart/home_navigator.dart
@@ -11,30 +11,37 @@ class HomeNavigator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Navigator(
-      key: NavigatorKeys.homeNavigatorKey,
-      initialRoute: "/",
-      onGenerateRoute: (settings) {
-        switch (settings.name) {
-          case "/":
-            return MaterialPageRoute(
-              builder: (context) {
-                return const HomeWidget();
-              },
-            );
-          case "product_view":
-            return MaterialPageRoute(
-                builder: (context) => ProductView(
-                      productItem: settings.arguments as Value,
-                    ));
-          default:
-            return MaterialPageRoute(
-                builder: (_) => Scaffold(
-                      body: Center(
-                          child: Text('No route defined for ${settings.name}')),
-                    ));
-        }
+    return WillPopScope(
+      onWillPop: () async {
+        NavigatorKeys.homeNavigatorKey.currentState?.maybePop();
+        return false;
       },
+      child: Navigator(
+        key: NavigatorKeys.homeNavigatorKey,
+        initialRoute: "/",
+        onGenerateRoute: (settings) {
+          switch (settings.name) {
+            case "/":
+              return MaterialPageRoute(
+                builder: (context) {
+                  return const HomeWidget();
+                },
+              );
+            case "product_view":
+              return MaterialPageRoute(
+                  builder: (context) => ProductView(
+                        productItem: settings.arguments as Value,
+                      ));
+            default:
+              return MaterialPageRoute(
+                  builder: (_) => Scaffold(
+                        body: Center(
+                            child:
+                                Text('No route defined for ${settings.name}')),
+                      ));
+          }
+        },
+      ),
     );
   }
 }
