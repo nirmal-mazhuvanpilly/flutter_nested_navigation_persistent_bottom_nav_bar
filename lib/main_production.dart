e5d8f1a9b364044326f0eaf2abaaa8275b0a628f
nirmal-mazhuvanpilly <nirmalmazhuvanpilly@gmail.com>
2022-05-23 11:00:17 +0530

    Build flavors for Android with different Firebase projects per flavor

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test_application/my_app.dart';
 
 import 'package:flutter_test_application/services/locators.dart';
 
+//copy google-services.json to debug,release,profile folders under each build flavor
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await setUpLocator(staging: false);
   runApp(const MyApp());
 }
